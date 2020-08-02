5e68880dbc60c136d42edd2e09db3f0cd69a3827
Ace Fox <afox09@gmail.com>
2020-08-02 18:12:27 -0500

    made my own youtube adapter funciton

---
+++ src/components/App.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import './App.css';
+//import ReactPlayer from 'react-player/youtube'
+import ListItem from './ListItem/ListItem';
+
+
+class App extends Component {
+  state = {
+    newUrl:'',
+    urlArray: []};
+
+  handleChange = (event) =>{
+  //  console.log('in handleChange', event.target.value);
+    this.setState({
+      ...this.state.url,
+      newUrl: event.target.value,
+    })
+
+  }//end handleChange
+
+  handleSubmit = () => {
+    console.log('in handleSubmit')
+    if (this.state.newUrl !== ''){
+      this.setState({
+        urlArray: this.state.urlArray.concat(this.state.newUrl),
+        newUrl: '',
+      })
+  }
+    console.log(this.state)
+  }//end handleSubmit
+
+  handleClear = () =>{
+    this.setState({
+      urlArray: [],
+      newUrl:'',
+    })
+  }
+
+  deleteFunction  = (index) => {
+    let array  =  this.state.urlArray;
+    array.splice(index, 1)
+    this.setState({
+      ...this.state.newUrl,
+      urlArray: array,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h3>enter a youtube url</h3>
+          <input value = {this.state.newUrl} onChange = {this.handleChange}/>
+          <br/>
+          <button onClick = {this.handleSubmit}>Add video to list!</button>
+          <br/>
+          {/* {JSON.stringify(this.state.urlArray)} */}
+          {this.state.urlArray.map((url, index)=>
+          <ListItem key = {index} url={url} index = {index} deleteFunction = {this.deleteFunction} />
+          )}
+        <br/>
+        <button onClick = {this.handleClear}>clear list</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ListItem/ListItem.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+//import ReactPlayer from 'react-player/youtube'
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+  });
+
+
+class ListItem extends Component {
+    state = {embedUrl:'https://www.youtube.com/embed/'}
+
+    handleDelete =()=>{
+        console.log('clicked delete item', this.props.index)
+        this.props.deleteFunction(this.props.index);
+    }
+    componentDidMount(){
+        let embedUrl = this.state.embedUrl
+        let inputUrl = this.props.url
+        for(let i =0; i<inputUrl.length; i++){
+            if ((inputUrl[i] === 'v') && (inputUrl[i+1]==='=')){
+               let importantBit = inputUrl.slice(i+2);
+               embedUrl += importantBit;
+            }//end if
+        }//end for  
+        console.log(embedUrl)
+        this.setState({embedUrl})
+    }
+ 
+  // Renders the entire app on the DOM
+  render() {
+    const {classes} = this.props;
+    return (
+        <>
+        <Grid container spacing={4}
+            direction="row"
+            justify="center"
+            alignItems="center">
+            <Grid item xs = {12} sm = {12} md={8} lg = {6}>
+                <Paper className={classes.paper}>
+                <iframe width="560" height="315" src={this.state.embedUrl} 
+                    frameBorder="0"
+                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                     allowFullScreen>
+                </iframe>
+                </Paper>
+            </Grid>
+            <Grid item xs = {12} sm={12} md ={4} lg = {6}>
+                <Paper className={classes.paper}>
+                    <p>Skate ipsum dolor sit amet, fakie out Zorlac impossible sponsored ollie north. 
+                        Tailslide dude hang up skate or die. 
+                        Fast plant ledge speed wobbles Vision lien air. 
+                        Mike York kick-nose downhill lien air boned out.
+                        </p>
+                    <button onClick = {this.handleDelete}>delete</button>
+                </Paper>
+            </Grid>
+        </Grid>
+        <br/>
+        <br/>
+    </>
+    );
+  }
+}
+
+ListItem.propTypes = {
+    classes: PropTypes.object.isRequired
+  };
+  
+  
+  export default withStyles(styles)(ListItem);
+  
